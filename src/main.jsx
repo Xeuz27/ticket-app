71155ee5d8550d4475cf5fba22bf4aef9d0017e8
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2023-07-18 17:03:00 -0400

    cleaning boilerplate code from vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import TicketApp from './TicketApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    <TicketApp />
   
 )
